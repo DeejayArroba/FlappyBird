d25577ab8617a678414403b74317d06952ffe0bd
Felipe <felipe.sacola@gmail.com>
2015-11-26 14:29:15 -0200

    Initial project generation

---
+++ desktop/src/me/djarroba/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.djarroba.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.djarroba.flappybird.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBird(), config);
+	}
+}
